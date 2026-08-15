3867b74f15a191a5cc7a4ddba731ac4bbb4a2639
nanhualyq <nanhualyq@gmail.com>
2026-08-15 21:45:27 +0800

    feat: update version to 0.0.18 and set manifest version in configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/auto-icons', '@wxt-dev/module-react'],
+  manifestVersion: 3,
   vite: () => ({
     plugins: [tailwindcss()],
   }),
